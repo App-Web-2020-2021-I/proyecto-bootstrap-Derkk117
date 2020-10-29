234eb3d9bedbbb6dccc26444ddfe4aa520923c23
derkk117 <angel.aguilar@111btec.com>
2020-10-29 11:49:10 -0600

    crear una cita terminado, responsivo

--- JS/appointments.js
+++ JS/appointments.js
@@ -1,15 +1,22 @@
-$(document).ready({
+$(document).ready(function() {
+    $("#CreateAppointmentForm").submit(function(e) {
+        e.preventDefault();
 
+        //Code...
+    })
 });
 
 function showModifyAppointment(list, create, modify) {
     $('#' + list)[0].style.display = "none";
+    $('#' + create)[0].style.display = "none";
 }
 
 function showCreateAppointment(list, create, modify) {
     $('#' + list)[0].style.display = "none";
+    $('#' + create)[0].style.display = "block";
 }
 
 function showAppointmentList(list, create, modify) {
     $('#' + list)[0].style.display = "block";
+    $('#' + create)[0].style.display = "none";
 }
