5115d6f45fb0b5e532f5f0fb0e38017b72f0c9be
derkk117 <angel.aguilar@111btec.com>
2020-10-31 21:22:13 -0600

    Aregar nuevo administrador terminado

---
+++ JS/addNewAdmin.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#ProfileForm").submit(function(e) {
+        e.preventDefault();
+
+        //Code...
+    });
+    $("#ProfileForm2").submit(function(e) {
+        e.preventDefault();
+
+        //Code...
+    });
+});
